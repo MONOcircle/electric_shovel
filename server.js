bcbdabb382eebf0822bfc05982feaab3f881dd4a
shoyoo <inami4321@gmail.com>
2019-06-15 17:20:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-﻿var WebSocketServer = require('ws').Server
-, wss = new WebSocketServer({port: 8124});
+var WebSocketServer = require('ws').Server
+, wss = new WebSocketServer({port: 8124})
+, var today = new Date();
 wss.on('connection', function(ws) {
 　　ws.on('message', function(message) {
 　　console.log('received: %s', message);
 });
-ws.send('something');
+ws.send(today);
+//ws.send('something');
 });
